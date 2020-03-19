efb09bdfe8d29859c7fd881672f32eca85ac4eb9
shintanabilla23 <nabillashinta168@gmail.com>
2020-03-19 22:37:18 +0700

    tugas class dan object

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TugasTaxi
+{
+    class Program //www.fckr3i.com
+    {
+		static void Main(string[] args)
+		{
+			// membuat objek dari class taxi
+
+			Taxi taxi = new Taxi();
+
+
+			// mengeset nilai properties
+			taxi.DriverName = "Untung Dwi Handoko";
+			taxi.OnDuty = true;
+			taxi.NumPassenger = 10;
+
+			// memanggil method 
+			taxi.TaxiInfo();
+			taxi.PickupPassenger();
+			taxi.DropOffPassenger();
+
+			Console.ReadKey();
+		}
+
+	}
+}
